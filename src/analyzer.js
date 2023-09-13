c574ec345bd8115d78a9c24a985482e730a1e7e4
Elizabeth <123765828+chabelina@users.noreply.github.com>
2023-09-12 20:40:07 -0500

    [FEATURE] Text Analyzer (#1)
    * Implementacion del metodo/funcionalidad getCharacterCount
    
    * Pequeña edicion al texto de la metrica caracteres
    
    * Implementacion del metodo/funcionalidad getCharacterCountExcludingSpaces
    
    * Implementacion de la funcionalidad del boton para Limpiar Texto
    
    * Implementacion del metodo/funcionalidad getWordCount
    
    * Agregando un if statement al metodo getWordCount para mostrar cero en la metrica cuando el textarea este vacio
    
    * Agregando tres eiquetas list en HTML para las metricas restantes (+ pequeñas correcciones semanticas)
    
    * Corrigiendo un error en la logica del metodo getCharacterCountExcludingSpaces
    
    * Implementacion del metodo/funcionalidad getNumberCount
    
    * Implementacion del metodo/funcionalidad getNumberSum
    
    * Implementacion del metodo/funcionalidad getAverageWordLength (+ pequeñas modificaciones)
    
    * Estilizando la pagina con CSS. Se agregó un stylesheet 'reset.cs' para limpiar la estilizacion por defecto de la pagina
    
    * Corrigiendo errores del test oas-css
    
    * Implementando el README de mi proyecto (+pequeño cambio de html semantico)
    
    * Corrigiendo el README

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,70 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const numPalabras = text.trim().split(" ").length;
+    if(text === ""){
+      return numPalabras - 1;
+    }
+    else return numPalabras;
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    //removiendo los espacios y signos
+    const textoSinEspaciosYSignos = text.replace(/[^\w\s]|_/g,'').replace(/\s+/g,'');
+    //contabilizando los caracterest restantes
+    const numCaracteres = textoSinEspaciosYSignos.length;
+    return numCaracteres;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    
+    /*removemos los simbolos y signos del texto (si es que los hay)
+    y usamos .split para obtener un array con strings separados por un
+    espacio (palabras)*/
+    const Palabras = text.trim().replace(/(?![.,])[^a-zA-Z0-9\s]|_/g,'').split(" ");
+    let sumaLongitudPalabras = 0;
+    
+    if(Palabras){
+      for(let i = 0; i < Palabras.length; i++){
+      //hacemos una suma iterativa con el valor .length de cada elemento y lo almacenamos en la variable 'sumaLongitudPalabras'
+        sumaLongitudPalabras += Palabras[i].length;
+      }
+      //creamos variable para hallar y almacenar la longitud promedio
+      const longitudPromedio = sumaLongitudPalabras/Palabras.length;
+      return parseFloat(longitudPromedio.toFixed(2));
+    }
+    else return 0;
+    //bucle 'for' para recorrer los elementos del array 'Palabras'
+    
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    //matching los numeros que se encuentran en el text input
+    const regex = /\b\d+(\.\d+)?(?![A-Za-z])\b/g ;
+    const Numeros = text.match(regex);
+
+    if (Numeros){
+      return Numeros.length
+    }
+    else return 0;
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const regex = /\b\d+(\.\d+)?(?![A-Za-z])\b/g ;
+    const Numeros = text.match(regex);
+    let suma = 0;
+
+    if (Numeros){
+      for(let i = 0; i < Numeros.length; i++){
+        suma += parseFloat(Numeros[i]);
+      }
+      return suma;
+    }
+    else return 0;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//Obtenemos el contenido que sea ingresado al textarea
+const textbox = document.querySelector('[name="user-input"]');
+//Obtenemos los elementos <li> y su valor
+const contadoresMetrica = document.getElementsByClassName("metrica");
+const botonReset = document.getElementById("reset-button");
+
+//Empezamos a monitorear eventos en el DOM
+//Monitoreamos el ingreso de characteres en tiempo real del textarea.
+textbox.addEventListener("keyup",actualizarMetricas);
+botonReset.addEventListener("click", limpiarTexto);
+
+//Funcion que actualiza las metricas tras la deteccion de un evento en tiempo real.
+function actualizarMetricas(){
+  //obtenemos la metrica de palabras con uso del objeto analyzer
+  const WordCount = analyzer.getWordCount(textbox.value);
+  contadoresMetrica[0].textContent = "Palabras: " + WordCount;
+  
+  //cuenta de characteres
+  //obtenemos la metrica de caracteres con uso del objeto analyzer
+  const CharCount = analyzer.getCharacterCount(textbox.value);
+  contadoresMetrica[1].textContent = "Caracteres: " + CharCount;
+  
+  //obtenemos la metrica de caracteres sin espacios o signos con uso del objeto analyzer
+  const CharCountExSpace = analyzer.getCharacterCountExcludingSpaces(textbox.value);
+  contadoresMetrica[2].textContent = "Caracteres Sin Espacios: " + CharCountExSpace;
+  
+  //obtenemos la metrica de numeros con uso del objeto analyzer
+  const NumCount = analyzer.getNumberCount(textbox.value);
+  contadoresMetrica[3].textContent = "Cantidad de Numeros: " + NumCount;
+  
+  //obtenemos la metrica de la suma de numeros con uso del objeto del objeto analyzer
+  const NumSum = analyzer.getNumberSum(textbox.value);
+  contadoresMetrica[4].textContent = "Suma de Numeros: " + NumSum;
+
+  //obtenemos la metrica de la longitud promedio de las palabras con uso del objeto analyzer
+  const AvrgWordLength = analyzer.getAverageWordLength(textbox.value);
+  contadoresMetrica[5].textContent = "Longitud Promedio de Palabras: " + AvrgWordLength;
+}
+
+function limpiarTexto(){
+  textbox.value = "";
+  actualizarMetricas();
+}
+
